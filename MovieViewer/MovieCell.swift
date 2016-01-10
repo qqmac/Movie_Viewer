c7862048520075cefa272b3e6558863a900e2375
Quyen Mac <QuyenMac@Quyens-MBP.home>
2016-01-09 21:15:29 -0500

    finished required steps

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Quyen Mac on 1/9/16.
+//  Copyright © 2016 Quyen Mac. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var refreshControl: UIRefreshControl!
 
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
         tableView.delegate = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -40,19 +48,32 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             
                             self.tableView.reloadData()
                             
+                            //self.refreshControl.endRefreshing()
+                            
                     }
                 }
         });
         task.resume()
         
         
-        
-        
-        
-        
         // Do any additional setup after loading the view.
     }
-
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() {
+        delay(2, closure: {
+            self.refreshControl.endRefreshing()
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -68,12 +89,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
         
-        cell.textLabel!.text = title
+        let baseUrl = "http://image.tmdb.org/t/p/w500/"
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
+        cell.posterView.setImageWithURL(imageUrl!)
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        //cell.textLabel!.text = title
         print("row \(indexPath.row)")
         return cell
     }
